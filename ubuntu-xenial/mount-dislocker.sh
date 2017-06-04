bb74916c20fbde12feeca48b01003c4925199566
Syakur Rahman <bS.xx7_37@yahoo.com>
2017-06-04 14:56:55 +0700

    Initial version. Should mount bitlocker drives statically

--- ubuntu-xenial/mount-dislocker.sh
+++ ubuntu-xenial/mount-dislocker.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 # This already assumes that dislocker is already installed and configured correctly.
+# It also assumes that the bitlocker disk are mounted to /dev/sdb5
+
+sudo umount /media/blmaster
+sudo dislocker -r -V /dev/sdb5 -u -- /media/blmaster
+cd /media/blmaster
+sudo mount -r -o loop dislocker-file /media/mountmaster
 
 
