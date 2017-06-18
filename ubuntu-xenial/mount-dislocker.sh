2bb853608fc0a4c298972d3b372c943adaea6f13
Syakur Rahman <bS.xx7_37@yahoo.com>
2017-06-18 17:00:55 +0700

    Add second partition to mount

--- ubuntu-xenial/mount-dislocker.sh
+++ ubuntu-xenial/mount-dislocker.sh
@@ -3,9 +3,16 @@
 # This already assumes that dislocker is already installed and configured correctly.
 # It also assumes that the bitlocker disk are mounted to /dev/sdb5
 
+mkdir /media/blmaster
+mkdir /media/mountmaster
 sudo umount /media/blmaster
 sudo dislocker -r -V /dev/sdb5 -u -- /media/blmaster
 cd /media/blmaster
 sudo mount -r -o loop dislocker-file /media/mountmaster
 
-
+mkdir /media/blblue
+mkdir /media/mountblue
+sudo umount /media/blblue
+sudo dislocker -r -V /dev/sdb7 -u -- /media/blblue
+cd /media/blblue
+sudo mount -r -o loop dislocker-file /media/mountblue
